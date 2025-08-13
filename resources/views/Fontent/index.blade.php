b125d11f2ed4f78c066f7f499ea22c6dc5835030
hardikcursor <cursorsoft776@gmail.com>
2025-08-13 14:18:11 +0530

    upto date

--- resources/views/Fontent/index.blade.php
+++ resources/views/Fontent/index.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-lg-9 col-md-10">
                     <div class="block">
                         <span class="d-block mb-3 text-white text-capitalize">Prepare for new future</span>
-                        <h1 class="animated fadeInUp mb-5">Our work is <br>presentation of our <br>capabilities.</h1>
+                        <h1 class="animated fadeInUp mb-5">A Complete Solution <br>Provider And System <br>Integrator.</h1>
                         <a href="#" class="btn btn-main animated fadeInUp btn-round-full" aria-label="Get started">Get
                             started<i class="btn-icon fa fa-angle-right ml-2"></i></a>
                     </div>
@@ -24,7 +24,7 @@
                 <div class="col-lg-8">
                     <div class="section-title">
                         <span class="h6 text-color ">We are creative & expert people</span>
-                        <h2 class="mt-3 content-title">We work with business & provide solution to client with their
+                        <h2 class="mt-3 content-title">We work with business & provide solution to Government/Semi Government Public Transport Operators with their
                             business problem
                         </h2>
                     </div>
